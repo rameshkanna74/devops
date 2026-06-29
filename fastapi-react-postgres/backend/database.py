568fb33d718a073967a91267da652f77d353b514
rameshkanna74 <rameshkanna725@gmail.com>
2026-06-29 14:30:30 +0000

    python volume 1

--- fastapi-react-postgres/backend/database.py
+++ fastapi-react-postgres/backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql://pg_user:pg_password@db:5432/fastapi_db",
+    "postgresql+psycopg://postgres:pg_password@db:5432/fastapi_db",
 )
 
 engine = create_engine(DATABASE_URL)
